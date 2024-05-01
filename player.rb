f2f3a652270f269f95d8ac1d76d3eee537fb4a96
Rcm <rcm68888@gmail.com>
2024-05-01 07:34:56 -0600

    the final player method

---
+++ player.rb
@@ -0,0 +1,16 @@
+class Player
+  attr_accessor :name, :lives  
+
+  def initialize(name)
+    @name = name 
+    @lives = 3
+  end
+
+  def lose_life
+    self.lives -= 1
+  end
+
+  def is_alive?
+    self.lives > 0
+  end
+end
